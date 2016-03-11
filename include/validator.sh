00feb3d6c8d9a3d6327de83bc879620c9604d2fd
Maciek Krawiec <maciek.krawiec@zoho.com>
2016-03-11 21:24:42 +0100

    Fix npkg_dir validator

--- include/validator.sh
+++ include/validator.sh
@@ -7,7 +7,7 @@ _validator_arg_sig() { [ ! -z ${ARG_SIG} ] || cmd_error 'Missing argument'; }
 
 _validator_pkg_dir() { [ -d ${PKG_DIR} ] || cmd_error "Package ${ARG_SIG} does not exist"; }
 
-_validator_npkg_dir() { [ -d ${PKG_DIR} ] && cmd_error "Package ${ARG_SIG} already exists"; }
+_validator_npkg_dir() { [ ! -d ${PKG_DIR} ] || cmd_error "Package ${ARG_SIG} already exists"; }
 
 _validator_target_flag() { [ ${ARG_FLAGS[t]+1} ] || cmd_error 'Missing target flag'; }
 
